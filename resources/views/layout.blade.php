61ee5b33c9a295abf63976417dffbd0560490e7f
kritsana <vatachijpop@gmail.com>
2017-02-04 19:49:43 +0700

    Add google analytic

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,15 @@
         <link rel="stylesheet" type="text/css" href="/css/main.css">
         <script src="/js/jquery-1.11.3.min.js" type="text/javascript" data-library="jquery" data-version="1.11.3"></script>
         <script src="/js/main.js" type="text/javascript" ></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-91398632-1', 'auto');
+          ga('send', 'pageview', location.pathname);
+        </script>
         @yield('header')
     </head>
     <body>
