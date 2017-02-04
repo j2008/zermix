ee23965885c1a17043aa5c795d19ffa22a9e3c5f
kritsana <vatachijpop@gmail.com>
2017-02-04 15:15:49 +0700

    Store page adjust map color

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -9,8 +9,7 @@
 
 @section('content')
   <div class="map">
-    <h2>เลือกภูมิภาคโดยคลิกที่แผนที่</h2>
-    <div style="border-bottom:1px solid #f1c5c1;margin:20px auto;width:70%;"></div>
+    <div class="map-title"><span class="title-h2"><h2>คลิกที่แผนที่เพื่อเลือกภาค</h2></span></div>
     <img src="img/map.png" usemap="#map">
     <map name="map">
       <area shape="poly" coords="2,100,40,29,145,2,209,43,198,143,143,172,126,136,109,164,59,165,2,100" onClick="openMap('north')" />
